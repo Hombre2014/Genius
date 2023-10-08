7f7db65b4f15c970743c1a23a59b172cff275842
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-10-08 11:57:42 +0200

    Video generation implemented.

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -2,7 +2,7 @@ import * as z from 'zod';
 
 export const formSchema = z.object({
   prompt: z.string().min(1, {
-    message: 'Image promt is required.',
+    message: 'Image prompt is required.',
   }),
   amount: z.string().min(1),
   resolution: z.string().min(1),
