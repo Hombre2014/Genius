fcc832b1118b93b6c048e070d9bf036b8dc02bc4
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-10-10 23:22:01 +0200

    Add premium button styling.

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -36,7 +36,7 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
               value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
             />
           </div>
-          <Button className="w-full">
+          <Button className="w-full" variant="premium">
             Upgrade
             <Zap className="w-4 h-4 ml-2 fil-white" />
           </Button>
