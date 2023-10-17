78711d823b4f5ad8f855a2ea70552de3a734ee5a
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-10-17 15:40:00 +0200

    Implemented subscription.

---
+++ components/subscription-button.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import axios from 'axios';
+import { Zap } from 'lucide-react';
+import { useState } from 'react';
+
+import { Button } from './ui/button';
+
+interface SubscriptionButtonProps {
+  isPro: boolean;
+}
+
+export const SubscriptionButton = ({
+  isPro = false,
+}: SubscriptionButtonProps) => {
+  const [loading, setLoading] = useState(false);
+  const onClick = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('/api/stripe');
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log('[BILLING_ERROR]', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Button
+      variant={isPro ? 'default' : 'premium'}
+      onClick={onClick}
+      disabled={loading}
+    >
+      {isPro ? 'Manage Subscription' : 'Upgrade to Pro'}
+      {!isPro && <Zap className="w-4 h-4 ml-2 fill-white" />}
+    </Button>
+  );
+};
